d8c6bd87f1c962973dd86622b7850fc110878afd
Alessandro <66976091+alessandromrc@users.noreply.github.com>
2023-04-15 22:59:24 +0200

    Now the messages should always be sent

--- ChatPOC.lua
+++ ChatPOC.lua
@@ -1,9 +1,8 @@
 local draft = nil
 local last_draft = nil
 local chat_open = false
-local mode = 0;
-local has_pressed_enter = false;
-
+local mode = 0
+local has_pressed_enter = false
 
 local send_chat_message = memory.scan("48 8D 15 ? ? ? ? 48 8D 4E 20 E8 ? ? ? ?")
 assert(send_chat_message ~= 0)
@@ -16,57 +15,54 @@ util.on_stop(function(_)
     memory.write_ubyte(send_chat_message + 0x3D, 0x74)
 end)
 
-while true do
-
-    -- Wait for chat to open
-    while not chat_open do
+local function chat_input()
+    while true do
         local state = chat.get_state()
-
         if state == 2 then
             draft = chat.get_draft()
             chat_open = true
             mode = 2
-        end
-
-        if state == 1 then
+            coroutine.yield()
+        elseif state == 1 then
             draft = chat.get_draft()
             chat_open = true
             mode = 1
+            coroutine.yield()
+        else
+            chat_open = false
+            coroutine.yield()
         end
-
-        has_pressed_enter = util.is_key_down(0x0D)
-
-        util.yield(100)
+        util.yield()
     end
+end
 
-    -- Wait for chat to close
-    while chat_open do
-        local state = chat.get_state()
+local chat_input_co = coroutine.create(chat_input)
+coroutine.resume(chat_input_co)
 
-        if state == 2 or state == 1 then
-            draft = chat.get_draft()
-        elseif state == 0 then
-            if has_pressed_enter then
-                last_draft = draft
-                util.toast(draft)
-                local pid = players.user()
-                for k,iter_pid in pairs(players.list(true, true, true)) do
-                    if iter_pid ~= pid then
-                        if mode == 1 then
+while true do
+    if chat_open then
+        has_pressed_enter = util.is_key_down(0x0D)
+        if has_pressed_enter then
+            last_draft = draft
+            util.toast(draft)
+            local pid = players.user()
+            for k, iter_pid in pairs(players.list(true, true, true)) do
+                if iter_pid ~= pid then
+                    if mode == 1 then
                         chat.send_targeted_message(iter_pid, pid, draft, true)
-                        else
-                          chat.send_targeted_message(iter_pid, pid, draft, false)
-                        end
+                    else
+                        chat.send_targeted_message(iter_pid, pid, draft, false)
                     end
                 end
             end
             chat_open = false
         end
-
-        has_pressed_enter = util.is_key_down(0x0D)
-
-        util.yield(100)
     end
+    has_pressed_enter = util.is_key_down(0x0D)
+    if not chat_open and has_pressed_enter then
+        coroutine.resume(chat_input_co)
+    end
+    util.yield()
 end
 
 util.keep_running()
